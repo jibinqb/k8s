11db275033d70b6b36cbbfecf6e40f685db0c9c4
jibinqb <67462398+jibinqb@users.noreply.github.com>
2022-08-19 01:34:14 +0530

    Create logs.go

---
+++ logs.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	GetPodLogs("i3-access-efded", "counter")
+}
+
+func GetPodLogs(namespace string, PodName string) error {
+	follow := true
+	count := int64(100)
+	podLogOptions := corev1.PodLogOptions{
+		Follow:    follow,
+		TailLines: &count,
+	}
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	podLogRequest := clientset.CoreV1().
+		Pods(namespace).
+		GetLogs(PodName, &podLogOptions)
+	stream, err := podLogRequest.Stream(context.TODO())
+	if err != nil {
+		return err
+	}
+	defer stream.Close()
+
+	for {
+		buf := make([]byte, 2000)
+		numBytes, err := stream.Read(buf)
+		if numBytes == 0 {
+			continue
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		message := string(buf[:numBytes])
+		fmt.Print(message)
+	}
+	return nil
+}
